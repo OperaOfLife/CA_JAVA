dc7c527a36b600fa6a7472bdbac91d520ff7505c
Genesis Tan <genesistanyx@u.nus.edu>
2021-06-28 11:28:12 +0800

    test commit

--- Team1_JAVA_CA/src/main/java/sg/edu/iss/caps/service/LecturerService.java
+++ Team1_JAVA_CA/src/main/java/sg/edu/iss/caps/service/LecturerService.java
@@ -3,11 +3,13 @@ package sg.edu.iss.caps.service;
 import java.util.ArrayList;
 
 import sg.edu.iss.caps.domain.Course;
+import sg.edu.iss.caps.domain.Enrolment;
 
+public interface LecturerService {
 
-public interface LecturerService 
-{
-
+	public Course findCourseById(int id);
+	public int findLecturerIdbyEmail(String email);
 	public ArrayList<Course> listAllcourses();
-	public Course findCourseById(String courseId);
+	public ArrayList<Course> findCourseNamesByLecturerId(int id);
+	public ArrayList<Enrolment>listEnrolmentByCourseId(int id);
 }
--- Team1_JAVA_CA/src/main/java/sg/edu/iss/caps/service/LecturerServiceImpl.java
+++ Team1_JAVA_CA/src/main/java/sg/edu/iss/caps/service/LecturerServiceImpl.java
@@ -7,24 +7,50 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import sg.edu.iss.caps.domain.Course;
+import sg.edu.iss.caps.domain.Enrolment;
+import sg.edu.iss.caps.repo.CourseLecturerRepository;
 import sg.edu.iss.caps.repo.CourseRepository;
+import sg.edu.iss.caps.repo.EnrolmentRepository;
+import sg.edu.iss.caps.repo.LecturerRepository;
 
 @Service
-public class LecturerServiceImpl implements LecturerService 
-{
+public class LecturerServiceImpl implements LecturerService {
 
+	
+	@Autowired
+	LecturerRepository lrepo;
+	
 	@Autowired
 	CourseRepository crepo;
 	
-	//Select all courses in course table 
+	@Autowired 
+	CourseLecturerRepository clrepo;	
+	
+	@Autowired
+	EnrolmentRepository erepo;
+	
+	@Transactional
+	public int findLecturerIdbyEmail(String email) {
+		return lrepo.findLecturerIdbyEmail(email);
+	}
+	
 	@Transactional
 	public ArrayList<Course> listAllcourses(){
 		return (ArrayList<Course>)crepo.findAll();
 	}
 	
-	//Select course name by course Id 
 	@Transactional
-	public Course findCourseById(String id) {
+	public Course findCourseById(int id) {
 		return crepo.findById(id).get();
+	}
+	
+	@Transactional 
+	public ArrayList<Course> findCourseNamesByLecturerId(int id) {
+		return clrepo.listCourseNamesByLecturerId(id);
+	}
+	
+	@Transactional
+	public ArrayList<Enrolment>listEnrolmentByCourseId(int id){
+		return erepo.findEnrolmentByCourseId(id);
 	}
 }
