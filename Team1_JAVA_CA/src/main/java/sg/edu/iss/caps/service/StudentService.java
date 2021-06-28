1025e8860e637391f349f317895f09177024457c
Genesis Tan <genesistanyx@u.nus.edu>
2021-06-28 11:32:33 +0800

    test

--- Team1_JAVA_CA/src/main/java/sg/edu/iss/caps/service/StudentService.java
+++ Team1_JAVA_CA/src/main/java/sg/edu/iss/caps/service/StudentService.java
@@ -1,10 +1,6 @@
 package sg.edu.iss.caps.service;
 
-import java.util.ArrayList;
-
-import sg.edu.iss.caps.domain.Enrolment;
 
 public interface StudentService {
 
-	public ArrayList<Enrolment>listEnrolmentByCourseId(String courseId);
 }
